29dd8501505df4ea513b7156a792f45b7e31b752
simbiote4 <garrett.a.hall2@gmail.com>
2025-10-22 17:01:34 -0500

    add 60fps limit

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None)
         pygame.display.flip()
-    
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        print(dt)
+
 
 if __name__ == "__main__":
     main()
