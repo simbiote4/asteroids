60c05db092f1f06d11c4de6781c4968cccca6ff3
simbiote4 <garrett.a.hall2@gmail.com>
2025-10-27 11:20:22 -0500

    draw player to screen

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None)
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
         print(dt)
+        
 
 
 if __name__ == "__main__":
